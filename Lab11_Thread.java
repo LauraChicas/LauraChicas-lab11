159d7d54a5ad4d5dae4c946195f402a2c8283c5d
LauraChicas <laurachicas@MacBook-Pro-de-Laura.local>
2025-04-03 19:36:49 -0400

    Test 2

--- Lab11_Thread.java
+++ Lab11_Thread.java
@@ -1,3 +1,36 @@
-public class Lab11_Thread {
-    
+import java.util.*;
+
+public class Lab11_Thread extends Thread {
+    private static ArrayList<String> data = new ArrayList<String>(); //test case 2
+    private String name;
+    private int runs; 
+
+    public Lab11_Thread(String name, int runs) {
+      this.name = name;
+      this.runs = runs;
+    }
+
+    public void run() {
+      for(int i = 0; i < runs; i++){
+        try{
+          Thread.sleep(50);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        this.addItem( name + " " + i);
+      }
+    }
+
+    public synchronized void addItem(String s){
+      System.out.println(s);
+      data.add(s);
+    }
+
+    public ArrayList<String> getData(){
+      return data;
+    }
+
+    public void setData(ArrayList<String> data){
+      this.data = data;
+    }
 }
